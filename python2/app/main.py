f59c110240c19aea20d9c6905233b7177c60c46c
Roman Dodin <dodin.roman@gmail.com>
2017-11-05 14:26:46 +0300

    initial commit

---
+++ python2/app/main.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return r"""
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+          </head>
+          <div class="container-fluid">
+            <h2>
+              <a id="Its_a_Dockerized_Flask_ADVANCED_0"></a>It’s a Dockerized Flask 
+              <code>[ADVANCED]</code>
+            </h2>
+            <a href="http://flask.pocoo.org/">
+              <img src="/static/docker_flask.png" alt="Flask">
+              </a>
+              <p class="lead">
+                <lead>It’s a Python2 
+                  <strong>Flask</strong> app deployed via 
+                  <strong>uWSGI</strong>, served by 
+                  <strong>Nginx</strong> and...
+                  <br> managed by 
+                    <strong>supervisord</strong> all in an 
+                    <strong>Alpine</strong> Docker container
+                  </p>
+                </div>
+"""
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
---
+++ python3/Dockerfile
@@ -0,0 +1,36 @@
+FROM alpine
+
+LABEL maintainer "Roman Dodin <dodin.roman@gmail.com>"
+LABEL description "Nginx + uWSGI + Flask based on Alpine Linux and managed by Supervisord"
+
+# Copy python requirements file
+COPY requirements.txt /tmp/requirements.txt
+
+RUN apk add --no-cache \
+    python3 \
+    bash \
+    nginx \
+    uwsgi \
+    uwsgi-python3 \
+    supervisor && \
+    python3 -m ensurepip && \
+    rm -r /usr/lib/python*/ensurepip && \
+    pip3 install --upgrade pip setuptools && \
+    pip3 install -r /tmp/requirements.txt && \
+    rm /etc/nginx/conf.d/default.conf && \
+    rm -r /root/.cache
+
+# Copy the Nginx global conf
+COPY nginx.conf /etc/nginx/
+# Copy the Flask Nginx site conf
+COPY flask-site-nginx.conf /etc/nginx/conf.d/
+# Copy the base uWSGI ini file to enable default dynamic uwsgi process number
+COPY uwsgi.ini /etc/uwsgi/
+# Custom Supervisord config
+COPY supervisord.conf /etc/supervisord.conf
+
+# Add demo app
+COPY ./app /app
+WORKDIR /app
+
+CMD ["/usr/bin/supervisord"]
